e67101083cf0636ade0ee00a70dcedfecd30ff3c
Layne Small <laynecsmall@gmail.com>
2016-11-21 17:45:14 +1300

    live update, titles etc

--- serial_rolling_avg.py
+++ serial_rolling_avg.py
@@ -5,7 +5,8 @@ print("ready to open")
 ser = serial.Serial('COM5')
 
 samples = []
-window = 3
+window = 4
+graph_window = 200
 
 ave_graph = []
 curr_graph = []
@@ -20,7 +21,6 @@ while True:
             current = int(line[1])
         except:
             continue
-        curr_graph.append(current)
         samples.append((current))
         if len(samples) > window:
             samples.pop(0)
@@ -29,16 +29,25 @@ while True:
         ave = total/window
         delta = ave - current
 
+        curr_graph.append(current)
         ave_graph.append(ave)
         del_graph.append(delta)
+
+        if len(curr_graph) > graph_window:
+            curr_graph.pop(0)
+            ave_graph.pop(0)
+            del_graph.pop(0)
         y_axis = range(0,len(ave_graph))
 
         print("curr: %d, ave: %d, delta: %d" % (current,ave, delta))
 
+        plt.clf()
         plt.subplot(211)
+        plt.title('Rolling average and current value')
         plt.plot(y_axis, curr_graph, 'r', y_axis, ave_graph,'b')
 
         plt.subplot(212)
+        plt.title('Delta between average and current')
         plt.plot(y_axis, del_graph, 'b')
         plt.pause(0.05)
 
